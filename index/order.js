d842d0514d97ce51228a1188975b037faf693a20
Taís Campos <112699437+TaisC468@users.noreply.github.com>
2023-01-27 11:49:45 -0300

    Update order.js
    Adicionando comandos por comentários e mudando o código.

--- index/order.js
+++ index/order.js
@@ -1,5 +1,10 @@
+//comandos para executar no terminal:
+//para iniciar o terminal: npm init
+//comando para instalar pacotes: npm install readline-sync
+//comando para rodar: node .\order.js
+
 import readlineSync from "readline-sync"
-const properties = [];
+const propriedades = [];
 let input = "";
 
 console.log("Digite as propriedades CSS. Digite 'SAIR' para finalizar.");
@@ -7,9 +12,9 @@ console.log("Digite as propriedades CSS. Digite 'SAIR' para finalizar.");
 while (input.toLowerCase() !== "sair") {
 input = readlineSync.question("Digite uma propriedade: ");
 if (input.toLowerCase() !== "sair") {
-properties.push(input);
+propriedades.push(input);
 }
 }
 
 console.log("Lista ordenada de propriedades CSS:")
-console.log(properties.sort().join("\n"));
+console.log(propriedades.sort().join("\n"));
