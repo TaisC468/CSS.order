c2d8eac28c31926fa22d03451975a8ab7c18dc51
Taís Campos <112699437+TaisC468@users.noreply.github.com>
2023-02-01 11:12:58 -0300

    Update index.js
    atualização de comentários.

--- index/index.js
+++ index/index.js
@@ -4,18 +4,23 @@
 //comando para instalar pacotes: npm install readline-sync
 //comando para rodar: node .\index.js
 
+
+//importando os pacotes e definindo 
 import readlineSync from "readline-sync"
 const propriedades = [];
 let input = "";
 
-console.log("Digite uma propriedade CSS ou digite 'SAIR' para finalizar.")
+//definindo mensagens que irão aparecer no terminal ao código.
 
+console.log("Digite uma propriedade CSS ou digite 'SAIR' para finalizar.")
+//enquanto a mensagem for diferente de 'SAIR' ele irá retornar a mensagem para digitar uma propriedade.
+//se a condição 1 for falsa o retorno será a lista de propriedades.
 while(input.toLowerCase()!=="sair"){
     input = readlineSync.question("Digite uma propriedade CSS:");
     if (input.toLowerCase()!=="sair"){
         propriedades.push(input);
     }
 }
-
+//o sort ordenará a lista ordenada.
 console.log("Lista ordenada de propriedades CSS:")
 console.log(propriedades.sort().join("\n"));
